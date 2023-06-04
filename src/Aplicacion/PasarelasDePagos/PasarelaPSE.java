86b74601f67d30d4c680fea51551571df1188612
Valeria Marin <valeriamarin193@gmail.com>
2023-06-04 18:48:36 -0500

    Implementación logs de transacciones para las pasarelas

--- src/Aplicacion/PasarelasDePagos/PasarelaPSE.java
+++ src/Aplicacion/PasarelasDePagos/PasarelaPSE.java
@@ -1,8 +1,14 @@
 package Aplicacion.PasarelasDePagos;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+
 public class PasarelaPSE implements PasarelaDePagos {
 
     private String nombrePasarela;
+    private BufferedWriter bw = null;
 
     PasarelaPSE() {
         this.nombrePasarela = "PSE";
@@ -19,8 +25,11 @@ public class PasarelaPSE implements PasarelaDePagos {
         boolean valida = validarTarjeta(tarjetaDeCredito);
         boolean tieneCupo = validarCupo(tarjetaDeCredito, monto);
         boolean noReportada = !validarReportada(tarjetaDeCredito);
+        boolean transaccionCompletada = valida && tieneCupo && noReportada;
 
-        return valida && tieneCupo && noReportada;
+        registrarTransaccion(tarjetaDeCredito, monto, transaccionCompletada);
+
+        return transaccionCompletada;
     }
 
     @Override
@@ -36,6 +45,32 @@ public class PasarelaPSE implements PasarelaDePagos {
     @Override
     public Boolean validarReportada(TarjetaDeCredito tarjetaDeCredito) {
         return !tarjetaDeCredito.isReportada();
+    }
+
+    @Override
+    public void registrarTransaccion(TarjetaDeCredito tarjetaDeCredito, Double monto, Boolean transaccionCompletada) {
+        // Registrar la transacción en un archivo txt
+        // Se registra la fecha, el monto y el número de la tarjeta
+
+        try {
+            bw = new BufferedWriter(new FileWriter("Data/Pagos/LogPSE.txt", true));
+            String estado = transaccionCompletada ? "completada" : "fallida";
+            bw.write(new Date(System.currentTimeMillis()) + " - " + estado + " - #Tarjeta: "
+                    + tarjetaDeCredito.getNumeroTarjeta() + " - Monto: " + monto + " COP");
+            bw.newLine();
+            bw.flush();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally
+
+        {
+            if (bw != null)
+                try {
+                    bw.close();
+                } catch (IOException ioe2) {
+
+                }
+        }
     }
 
 }
