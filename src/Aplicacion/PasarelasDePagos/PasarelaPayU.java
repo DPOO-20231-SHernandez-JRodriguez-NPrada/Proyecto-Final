dc4a6cceddda54409601573ef8aceef40bed97fa
Valeria Marin <valeriamarin193@gmail.com>
2023-06-04 15:46:27 -0500

    Pasarela PayU con métodos overriden

---
+++ src/Aplicacion/PasarelasDePagos/PasarelaPayU.java
@@ -0,0 +1,43 @@
+package Aplicacion.PasarelasDePagos;
+
+public class PasarelaPayU implements PasarelaDePagos {
+
+    private String nombrePasarela;
+
+    PasarelaPayU() {
+        this.nombrePasarela = "PayU";
+    }
+
+    public String getNombrePasarela() {
+        return nombrePasarela;
+    }
+
+    @Override
+    public Boolean procesarPago(TarjetaDeCredito tarjetaDeCredito, Double monto) {
+        // Si la tarjeta es válida, tiene cupo suficiente y no está reportada, se
+        // procesa el pago
+
+        if (validarTarjeta(tarjetaDeCredito) && validarCupo(tarjetaDeCredito, monto)
+                && !validarReportada(tarjetaDeCredito)) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+    @Override
+    public Boolean validarTarjeta(TarjetaDeCredito tarjetaDeCredito) {
+        return tarjetaDeCredito.validarTarjeta();
+    }
+
+    @Override
+    public Boolean validarCupo(TarjetaDeCredito tarjetaDeCredito, Double monto) {
+        return tarjetaDeCredito.validarCupo(monto);
+    }
+
+    @Override
+    public Boolean validarReportada(TarjetaDeCredito tarjetaDeCredito) {
+        return !tarjetaDeCredito.isReportada();
+    }
+}
